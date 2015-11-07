8fb9b46a5c6470c31dc1f5a0e429dd103bb19028
Ryan Moniz <ryanmoniz@gmail.com>
2015-11-06 21:44:33 -0700

    Initial Commit

---
+++ ConstraintsIdentifier/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ConstraintsIdentifier
+//
+//  Created by Ryan Moniz on 2015-11-06.
+//  Copyright © 2015 Ryan Moniz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ConstraintsIdentifier/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ConstraintsIdentifier
+//
+//  Created by Ryan Moniz on 2015-11-06.
+//  Copyright © 2015 Ryan Moniz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
